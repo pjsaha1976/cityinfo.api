0eafe058b1b12e4dd2291c8c93ef1e913cf881d8
prosenjit saha <pjsaha1976@gmail.com>
2025-03-23 18:00:32 -0700

    remove app build and deployment workflow

--- obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05dfe985e55c032b98450a7609fbeb15a23ba02f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a94fa2920a7a9610813b656381ff2979c98b4801")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
